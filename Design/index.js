a772c555e7e327faa082956d3f760c856ba8353f
khushi-2002 <khushiagarwal915@gmail.com>
2022-12-22 20:23:40 +0530

    Added

--- Design/index.js
+++ Design/index.js
@@ -38,3 +38,13 @@ $(".blog-cards").owlCarousel({
    }
 
 });
+
+var sidemenu = document.getElementById("sidemenu")
+
+function openmenu(){
+    sidemenu.style.right = 0;
+}
+
+function closemenu(){
+    sidemenu.style.right= "-200px";
+}
