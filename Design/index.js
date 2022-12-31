4965585976df9fff17130eeadf955789327c0fff
shraddha7402 <hajarishraddha519@gmail.com>
2022-12-31 19:53:24 +0530

    adding

--- Design/index.js
+++ Design/index.js
@@ -48,3 +48,21 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right= "-200px";
 }
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
